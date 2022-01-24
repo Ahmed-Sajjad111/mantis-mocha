fa169413522ee4d14407c2dc7d91484030064982
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-24 17:53:25 -0600

    added Product schema to models

---
+++ server/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
