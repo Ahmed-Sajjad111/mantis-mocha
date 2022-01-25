edca0e8c19086f0d573c58980c9f938f7ec75228
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-24 18:14:11 -0600

    added category relationship to product

--- server/models/Product.js
+++ server/models/Product.js
@@ -23,6 +23,11 @@ const productSchema = new Schema({
         min: 0,
         default: 0
     },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
 });
 
 const Product = mongoose.model('Product', productSchema);
