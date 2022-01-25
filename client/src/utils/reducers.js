3c1c85d56b5cbbb90ad6dd5da2a598032e27baf1
Collin-W <whalencollin@gmail.com>
2022-01-25 13:33:08 -0600

    created actions and reducers, special reducers need more work still

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,106 @@
+import {
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+  TOGGLE_CART,
+  UPDATE_CART_QUANTITY,
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  RESET_ALL_CATEGORY,
+  CHANGE_PAGE_THEME,
+} from "./actions";
+
+//add state code here
+
+//
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    case RESET_ALL_CATEGORY :
+      return {
+        ...state,
+        // might need to change this
+        currentCategory: action.currentCategory,
+      };
+
+    case CHANGE_PAGE_THEME:
+      return {
+        ...state,
+       //code for this reducer
+      };
+
+    
+
+    default:
+      return state;
+  }
+};
