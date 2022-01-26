52539a479b2d8f883a29a64b3fdd82dc2ab92ad0
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-01-26 14:08:09 -0500

    added conditional statements to render specific functionality to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Auth from "../../utils/auth";
 import { AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
 
 function Nav() {
-    return (
-        <AppBar position="fixed" color="secondary" sx={{ mt: 9, width: "100%" }}>
-            <Grid>
+
+    function navHandler() {
+        if (Auth.loggedIn()) {
+            return (
                 <Toolbar sx={{ display: "flex", justifyContent: "flex-end" }}>
                     <Button color="success">
                         <Typography textAlign="center">Order History</Typography>
                     </Button>
+                    <Button href='/' onClick={() => Auth.logout()} color="success">
+                        <Typography textAlign="center">Logout</Typography>
+                    </Button>
+                </Toolbar>
+            )
+        } else {
+            return (
+                <Toolbar sx={{ display: "flex", justifyContent: "flex-end" }}>
                     <Button href='/login' color="success">
                         <Typography textAlign="center">Login</Typography>
                     </Button>
@@ -16,6 +26,13 @@ function Nav() {
                         <Typography textAlign="center">Sign-Up</Typography>
                     </Button>
                 </Toolbar>
+            )
+        }
+    }
+    return (
+        <AppBar position="fixed" color="secondary" sx={{ mt: 9, width: "100%" }}>
+            <Grid>
+                {navHandler()}
             </Grid>
         </AppBar>
     );
