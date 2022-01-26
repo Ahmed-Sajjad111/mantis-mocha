2aa63918714d8a3dfa46150beb7806a371c17a0f
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-01-26 14:00:57 -0500

    removed unused variables

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
 
-// const pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
-
 function Nav() {
     return (
         <AppBar position="fixed" color="secondary" sx={{ mt: 9, width: "100%" }}>
