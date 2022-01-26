317e7e81250ff6b4c61fa56af2583d6bea491166
Mark <mjbc53@gmail.com>
2022-01-25 18:35:11 -0600

    added query to front end to get products by category, name or all products

---
+++ client/src/utils/queries.js
@@ -0,0 +1,52 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_GET_PRODUCTS_BY_CATEGORY= gql`
+  query getProductsByCategory($category: ID){
+  products(category: $category){
+    _id
+    name
+    description
+    image
+    price
+    quantity
+    category {
+      _id
+      name
+    }
+  }
+}
+`
+
+export const QUERY_GET_PRODUCTS_BY_NAME = gql`
+  query getProductsByName($name: String){
+    products(name: $name){
+      _id
+      name
+      description
+      image
+      price
+      quantity
+      category {
+        _id
+        name
+      }
+    }
+  }
+`
+
+export const QUERY_ALL_PRODUCTS = gql`
+  query{
+    products{
+      _id
+      name
+      description
+      image
+      price
+      quantity
+      category {
+        _id
+        name
+      }
+    }
+  }
+`
