3297deb73ee6acef9e753985f5e855dff442f6ab
Mark <mjbc53@gmail.com>
2022-01-31 17:22:32 -0600

    changed expiration in the auth.js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const secret = 'mysecretsshhhhh';
-const expiration = '20000';
+const expiration = '2h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
