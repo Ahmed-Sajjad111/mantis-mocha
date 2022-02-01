b4a77acb22ee0800a83581855487f2b6a14af481
Mark <mjbc53@gmail.com>
2022-01-31 18:43:24 -0600

    added comments to the graphql routes and the server.js file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,16 +4,21 @@ const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
+  // queries
   Query: {
+    // get all categories 
     categories: async() => {
       return await Category.find()
     },
+    // get a sinlge category of products
     category: async (parent, { _id }) => {
       return await Category.findOne({_id})
     },
+    // get sinlge product
     product: async(parent, {_id}) => {
       return await Product.findOne({_id})
     },
+    //get all products
     products: async (parent, {category, name}) => {
       const params = {}
 
@@ -29,6 +34,7 @@ const resolvers = {
 
       return await Product.find(params).populate('category')
     },
+    //get a shoppers info
     shopper: async (parent, args, context) => {
       if(context.shopper) {
         const shopper = await Shopper.findById(context.user._id).populate({
@@ -41,6 +47,7 @@ const resolvers = {
       
       throw new AuthenticationError('Not logged in')
     },
+    //get an orders info
     order: async (parent, { _id }, context) => {
       if (context.shopper){
         const shopper = await Shopper.findById(context.user._id).populate({
@@ -53,14 +60,19 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in')
     },
+    //use stripes to generate products and check out
     checkout: async (parent, args, context) => {
+      //get the url of the page
       const url = new URL(context.headers.referer).origin;
 
-
+      //create new order with products
       const order = new Order ({products: args.products})
+      //get get products from order
       const { products } = await order.populate('products').execPopulate()
+      //place holder array 
       const line_items = []
 
+      //loop through and create products in stripe
       for(let i = 0; i < products.length; i++){
         //generate product id
         const product = await stripe.products.create({
@@ -75,12 +87,14 @@ const resolvers = {
           currency: 'usd',
         });
 
+        //push to the line_items array
         line_items.push({
           price: price.id,
           quantity: 1
         })
       }
 
+      //create session for stripe
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items,
@@ -92,13 +106,16 @@ const resolvers = {
       return {session: session.id}
     }
   },
+  //mutations
   Mutation: {
+    //add shopper
     addShopper: async (parent, args) => {
       const shopper = await Shopper.create(args);
       const token = signToken(shopper);
 
       return { token, shopper };
     },
+    //add order
     addOrder: async (parent, { products }, context) => {
       if (context.shopper) {
         const order = new Order({ products });
@@ -110,6 +127,7 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
+    //update shoppers info
     updateShopper: async (parent, args, context) => {
       if (context.shopper) {
         return await Shopper.findByIdAndUpdate(context.shopper._id, args, { new: true });
@@ -117,9 +135,11 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
+    //update the products quantity
     updateProductQuantity: async (parent, { _id, quantity }) => {
       return await Product.findByIdAndUpdate(_id, {quantity: quantity}, { new: true });
     },
+    //login
     login: async (parent, { email, password }) => {
       const shopper = await Shopper.findOne({ email });
 
