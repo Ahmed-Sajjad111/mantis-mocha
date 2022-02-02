bca6301b8e112f720b720e305b8510dc9e734968
BenPaulat <benjamin.d.paulat@gmail.com>
2022-02-01 21:11:45 -0600

    made footer container responsive

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
     return (
         <Paper sx={{ bottom: 0, position: "fixed", width: "100%", right: 0, bgcolor: 'primary.main', }}>
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={7}>
+                <Grid item xs={8}>
                     <Typography sx={{ fontSize: { xs:'caption.fontSize', sm:'h4.fontSize' }}} mt={1} ml={.5} pr={-2}>
                         2022 The Coffee Shop <span role="img" aria-label="coffee emoji">☕</span> , Inc.
                     </Typography>
                 </Grid>
-                <Grid item xs={12} sm={5}>
-                    <Stack direction="row" spacing={2} sx={{ justifyContent: "right" }} mt={1} mr={1}>
+                <Grid item xs={4}>
+                    <Stack direction="row" spacing={.25} sx={{ mt:{xs: 0, sm: 1}, justifyContent: "right" }} mt={1} mr={1}>
                         <a href="https://github.com/mjbc53" target="_blank" rel="noopener noreferrer">
                           <Avatar alt="Mark" src="https://avatars.githubusercontent.com/u/87452989?v=4" />
                         </a>
